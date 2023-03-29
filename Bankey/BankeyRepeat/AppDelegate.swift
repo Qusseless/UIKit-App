6df5a0437e7aa3f6ae44719c3aa3bda9f0671c78
Ilya Volkorez <qusseless@ilyas-mbp.home>
2023-03-29 17:22:35 +0200

    feat: Add tableView

--- Bankey/BankeyRepeat/AppDelegate.swift
+++ Bankey/BankeyRepeat/AppDelegate.swift
@@ -21,7 +21,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         onboardingContainerViewController.delegate = self
         dummyViewController.logoutDelegate = self
         
-        window?.rootViewController = mainViewController
+        window?.rootViewController = AccountSummaryViewController()
+        //window?.rootViewController = mainViewController
         //window?.rootViewController = loginViewController
         //window?.rootViewController = onboardingContainerViewController
         //window?.rootViewController = OnboardingContainerViewController()
